49808359eee385cad4d1c8cddcc039f14f5777c0
Neeraj Pratap Singh <nprataps@iitk.ac.in>
2022-05-11 06:28:16 +0530

    another commit

--- controllers/getBooks.js
+++ controllers/getBooks.js
@@ -4,14 +4,15 @@ const Books = require('../models/books.js');
 function getBooks(request, response) {
     try {
         let page = +request.query.page;
-        let size = Math.min(request.query.size, 5);
+        let skip = Math.max(5*(page-1),0)
+        let size = Math.min((+request.query.size)?request.query.size:5, 5);
 
-        Books.find({},{ _id: 0, __v:0 },{ skip: 5*(page-1), limit: size }, function (err, result) {
+        Books.find({},{ _id: 0, __v:0 },{ skip: skip, limit: size }, function (err, result) {
             if (err) throw err;
             if (result) {
                 let res= {
-                    page: page,
-                    items: size,
+                    page: (page)?page:1,
+                    items: (page && size)?size:5,
                     books: result
                 }
                 response.json(res)
